bba9b22a25b45b2152d7777aab697fe95e82aaa2
poper12 <samyakdhengle89@gmail.com>
2025-06-27 03:08:42 +0530

    config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ env_vars = {
     "API_HASH": os.getenv("API_HASH", ""),
     "API_ID": os.getenv("API_ID", ""),
     "BOT_TOKEN": os.getenv("BOT_TOKEN", ""),
-    "MONGO_URI": os.getenv("MONGO_URI", ""),  # ✅ Add this
+    "MONGO_URI": os.getenv("MONGO_URI", "mongodb+srv://Yuuichi:Yuuichi@cluster0.z3hyhbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),  # ✅ Add this
     "CACHE_CHANNEL": os.getenv("CACHE_CHANNEL", "Dump2075"),
-    "CHANNEL": os.getenv("CHANNEL", ""),
+    "CHANNEL": os.getenv("CHANNEL", "Manga_Campus"),
     "FNAME": os.getenv("FNAME", "[MC] [{chap_num}] {chap_name} @Manga_Campus")
 }
 
