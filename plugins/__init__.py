7967db0d882d358ac1bbafc95289d511059a55db
poper12 <samyakdhengle89@gmail.com>
2025-06-26 23:57:11 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,15 +3,16 @@ from .client import MangaClient, MangaCard, MangaChapter
 #Working Clients 
 from .mangadex import MangaDexClient
 from .mangabuddy import MangaBuddyClient
-from .reaperscans import ReaperScansClient
-from .asurascans import AsuraScansClient
 from .mgecko import MgekoClient
 from .comick import ComickClient
 from .manhwaclan import ManhwaClanClient
 from .mangamob import mangamob
+from .manhuafast import ManhuaFastClient
+#from .reaperscans import ReaperScansClient
+#from .asurascans import AsuraScansClient
 
 #18+ Client
 from .mangadistrict import MangaDistrictClient
 from .manga18fx import Manga18fxClient
-from .manhuafast import ManhuaFastClient
 from .omegascans import OmgeaScansClient
+from .manhwa18cc import Manhwa18ccClient
