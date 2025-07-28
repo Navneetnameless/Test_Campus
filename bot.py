aa2f6e548c7934d860a5cf990bc54298c595c34e
Navneetnameless <navneetsuppin12345@gmail.com>
2025-07-29 04:35:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ from tools.aqueue import AQueue
 from tools.flood import retry_on_flood
 
 
-OWNER_ID = 5543390445 # put owner id in number directly 
-auth_users = [5543390445, 5164955785, 5891177226, 7827086839, 6975428639] # eg: [83528911,836289,9362891] # eg: [83528911,836289,9362891]
+OWNER_ID = 1302933634 # put owner id in number directly 
+auth_users = [1302933634, 5164955785, 5891177226, 7827086839, 6975428639] # eg: [83528911,836289,9362891] # eg: [83528911,836289,9362891]
 AUTH_USERS = auth_users + [OWNER_ID]
 
 
